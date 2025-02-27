7d6480b96c54b8df6fb22e685b2f7dcf89c0467e
abdul alim <alimsujon12@gmail.com>
2025-02-27 11:26:15 +0600

    all exception error filter created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { MyLoggerModule } from './my-logger/my-logger.module';
 
 @Module({
   imports: [
@@ -16,10 +15,17 @@ import { APP_GUARD } from '@nestjs/core';
     EmployeesModule,
     ThrottlerModule.forRoot([
       {
+        name: 'short',
         ttl: 60000,
         limit: 3,
       },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
     ]),
+    MyLoggerModule,
   ],
   controllers: [AppController],
   providers: [
